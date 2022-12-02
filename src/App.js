8c43e3e6cf813fe7bee06365a783a5c2e7cfd8af
mohd zubair <mdzubair3782@gmail.com>
2022-12-02 23:51:45 +0530

    adding collegeinput

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import UsersList from './components/Users/UsersList';
 function App() {
   const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) => {
+  const addUserHandler = (uName, uAge,ucolllege) => {
     setUsersList((prevUsersList) => {
       return [
         ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
+        { name: uName, age: uAge,college:ucolllege, id: Math.random().toString() },
       ];
     });
   };
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useRef } from 'react';
 
 import Card from '.././Users/UI/Card';
 import Button from '.././Users/UI/Button';
@@ -6,38 +6,53 @@ import ErrorModal from '.././Users/UI/ErrorModal';
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+
+const nameInputRef=useRef();
+const ageInputRef=useRef();
+const collegeInputref=useRef()
+
+  // const [enteredUsername, setEnteredUsername] = useState('');
+  // const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    // console.log(nameInputRef.current.value)
+    // console.log(ageInputRef.current.value)
+    const enteredName=nameInputRef.current.value
+    const enteredUserAge=ageInputRef.current.value
+    const enteredUsercollege=collegeInputref.current.value
+
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0 || enteredUsercollege.trim().length===0 ) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
+    // props.onAddUser(enteredUsername, enteredAge);
+    props.onAddUser(enteredName, enteredUserAge,enteredUsercollege);
+    nameInputRef.current.value='';
+    nameInputRef.current.value='';
+    collegeInputref.current.value='';
+    // setEnteredUsername('');
+    // setEnteredAge('');
   };
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
+  // const usernameChangeHandler = (event) => {
+  //   setEnteredUsername(event.target.value);
+  // };
 
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
 
   const errorHandler = () => {
     setError(null);
@@ -58,15 +73,25 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            // value={enteredUsername}
+            // onChange={usernameChangeHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            // value={enteredAge}
+            // onChange={ageChangeHandler}
+            ref={ageInputRef}
+          />
+          <label htmlFor="collegename">College Name</label>
+          <input
+            id="college"
+            type="text"
+            // value={enteredAge}
+            // onChange={ageChangeHandler}
+            ref={collegeInputref}
           />
           <Button type="submit">Add User</Button>
         </form>
